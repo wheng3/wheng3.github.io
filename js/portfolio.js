9fb43d6af4cabf1906b74b53d57e90bf4ab22baf
Wee Jing Heng <wheng3@illinois.edu>
2017-08-29 13:22:30 +0800

    doge

---
+++ js/portfolio.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('#blue-btn').on('click', function() {
+    $('body').css('background', '#4286f4')
+  })
+  
+  $('#green-btn').on('click', function() {
+    $('body').css('background', '#36db6d')
+  })
+  
+  $('#red-btn').on('click', function() {
+    $('body').css('background', '#d63131')
+  })
+})
